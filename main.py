b657644074a4065154605a32910f28ad7cae4e4e
mustafa-senyuz <mustafasenyuz.git@gmail.com>
2025-05-15 15:19:37 +0000

    Auto update from script

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(storage=MemoryStorage())
 
+
 @dp.message()
 async def echo_handler(message: Message) -> None:
     await message.answer(f"Echo: {message.text}")
 
+
 async def main():
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
